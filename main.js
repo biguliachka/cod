8c56da56acbc65ceeee3a4cc55606996671e7afe
biguliachka <109332959+biguliachka@users.noreply.github.com>
2022-10-28 14:05:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function one(){
+document.getElementById('text').innerHTML+='1'
+}
+
+function two(){
+    document.getElementById('text').innerHTML+='2'
+}
+
+function three(){
+        document.getElementById('text').innerHTML+='3'
+        }
+ function four(){
+            document.getElementById('text').innerHTML+='4'
+            }
+
+function five(){
+                document.getElementById('text').innerHTML+='5'
+     }
+     function six(){
+        document.getElementById('text').innerHTML+='6'
+}
+function seven(){
+    document.getElementById('text').innerHTML+='7'
+}
+function eight(){
+    document.getElementById('text').innerHTML+='8'
+}
+     
+function nine(){
+    document.getElementById('text').innerHTML+='9'
+}
+function numberNull(){
+    document.getElementById('text').innerHTML+='0'
+}
+
+function enter(){
+    alert(document.getElementById('text').innerHTML)
+}
+
+function clearText(){
+document.getElementById('text').innerHTML=''
+}
+
+function cancelText() {
+ console.log(document.getElementById('text').textContent);
+}
+        
